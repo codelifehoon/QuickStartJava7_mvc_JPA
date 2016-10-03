533138c1ba7f378affb2c0d7e999bad62c01801b
장재훈 <codelife@11st.co.kr>
2016-10-03 21:47:52 +0900

    first commit

---
+++ src/test/java/com/example/QuickStartJava7ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QuickStartJava7Application.class)
+@WebAppConfiguration
+public class QuickStartJava7ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
